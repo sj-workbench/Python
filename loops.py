26388e57a218479e1f3a6856cbfbb6b6311d6883
git-sjworks <work.contactsuryanshu@gmail.com>
2026-06-07 19:26:02 +0530

    add new file

---
+++ loops.py
@@ -0,0 +1,14 @@
+# FOR LOOPS
+# range() function = range(start, stop, step)
+for i in range(10,21,1):
+    print(i)
+    
+n = int(input("Enter your number: "))
+for i in range(n,(n*10)+1, n):
+    print(i)
+
+
+
+
+
+# WHILE LOOPS
