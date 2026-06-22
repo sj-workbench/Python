74e9fd5add774326cd77880c5bcbcfd1925fbcc0
git-sjworks <work.contactsuryanshu@gmail.com>
2026-06-22 18:50:32 +0530

    Update all files

---
+++ All_learnt_things/docstring.py
@@ -0,0 +1,6 @@
+def square(n):
+    '''this is a square'''    # Doc string,not comment, doc string comes after function name, method, class or modules
+    print(n**2)
+square(5)
+print(square.__doc__)
+
