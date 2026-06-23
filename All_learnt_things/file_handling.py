ad2f3cc38968cb6c72b4ab053806c5099e8a9ff1
git-sjworks <work.contactsuryanshu@gmail.com>
2026-06-23 21:54:04 +0530

    Update all files

--- All_learnt_things/file_handling.py
+++ All_learnt_things/file_handling.py
@@ -1,6 +1,9 @@
-# read, append, write, create, text
+# read, append, write, create, text, binary
 
 f = open("myfile.txt","r")
 text = f.read()
 print(text)
 f.close()
+
+f = open("myfile2.txt","w")
+f.write("yo")
